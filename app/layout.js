5d6cdbf7163bb32a6e5bf957e6a97aa8c265555e
Aatreyee Joshi <136146209+Aatreyee23@users.noreply.github.com>
2024-09-05 15:16:12 +0530

    Added:
    Clerk Sign in and Authentication
    Dashboard

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Self Appraise",
+  description: "A webapp for self appraisal automation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+        <ClerkProvider>
+    <html lang="en">
+      
+      <body className={inter.className}>
+        
+          {children}
+      </body>
+    </html>
+        </ClerkProvider>
+  );
+}
